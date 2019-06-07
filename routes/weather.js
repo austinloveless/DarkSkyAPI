90795caf257e8b931d4dac95554a333b9cb4a320
Ciaran <cguhagil@terpmail.umd.edu>
2019-06-07 11:32:42 -0600

    fixed getaverage function bug

--- routes/weather.js
+++ routes/weather.js
@@ -4,36 +4,14 @@ const axios = require('axios');
 
 let nowUnix = Math.floor(Date.now()/1000);
 let hourAgoUnix = Math.floor(Date.now()/1000) - 3600;
+let minuteData;
 
 let apiUrl = process.env.APIURL || `https://api.darksky.net/forecast/abd6b9a3a3a391da0e1cbac33d52e1e8/39.7393,-104.9848,${hourAgoUnix}`;
-
-function getAverage() {
-    axios.get(apiUrl)
-    .then(response =>  response.data.minutely.data)
-    .then(response => lala(response))
-
-    function lala(data) {
-        let sum = 0
-        let avg = 0
-        
-        for (i = 0; i < data.length; i++) {
-            sum = sum + data[i]
-            console.log(sum)
-            avg = sum/data.length
-        }
-
-        if (avg === 0) {
-
-        }
-        else {
-
-        }
-    }
-}
 
 router.get('/', (req, res) => {
     axios.get(apiUrl)
     .then(response =>  response.data.minutely.data)
+    .then(response =>  console.log(response))
     .then(response =>  res.status(200).json({response}))
 
 })
@@ -41,9 +19,10 @@ router.get('/', (req, res) => {
 router.get('/lasthour', (req, res) => {
     axios.get(apiUrl)
     .then(response =>  response.data.minutely.data)
-    .then(response => getAverage(response))
+    .then(response => getAverage(response))    
 
     function getAverage(data) {
+
         let precipProbabilitySum = 0
         let precipProbabilityAvg = 0
 
@@ -68,8 +47,6 @@ router.get('/lasthour', (req, res) => {
     }
 
 })
-
-getAverage();
 
 
 
